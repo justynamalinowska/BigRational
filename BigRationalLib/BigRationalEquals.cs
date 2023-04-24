5aba743b48991196687c844126436dadd2f4a811
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-04-24 20:58:06 +0200

    Add Equals, operator “==“ and “!=“

---
+++ BigRationalLib/BigRationalEquals.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace BigRationalLib
+{
+	public readonly partial struct BigRational :IEquatable<BigRational>
+	{
+        //equals,== i !=
+
+        public bool Equals(BigRational other)
+        {
+            if (IsNaN(this) && IsNaN(other))
+                return true;
+            if (IsNaN(this) && !IsNaN(other) || !IsNaN(this) && IsNaN(other))
+                return false;
+            else
+                return (this.Numerator == other.Numerator && this.Denominator == other.Denominator);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj is null )
+            if(obj is BigRational other) return Equals(other);
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Numerator, Denominator);
+        }
+
+        public static bool operator ==(BigRational b1, BigRational b2)
+        { return b1.Equals(b2); }
+
+        public static bool operator !=(BigRational b1, BigRational b2)
+        { return !(b1.Equals(b2)); }
+    }
+}
+
