975609b399d76a7be1e6e3cfb8bc256501bf18b9
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-04-25 09:49:06 +0200

    Create methods for arithmetics operators

---
+++ BigRationalLib/BigRationalArithemtics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace BigRationalLib
+{
+    public readonly partial struct BigRational
+    {
+        public static BigRational operator +(BigRational left, BigRational right)
+        {
+            if (left == NaN || right == NaN)
+                return NaN;
+
+            //ToDO - sytuacje specjalne
+
+            return new(left.Numerator * right.Denominator + right.Numerator * left.Denominator,
+                left.Denominator * right.Denominator);
+        }
+
+        public static BigRational operator ++(BigRational b) => b + new BigRational(1);
+
+        public static BigRational operator -(BigRational left, BigRational right)
+        {
+            if (left == NaN || right == NaN)
+                return NaN;
+
+            //ToDO - sytuacje specjalne
+
+            return new(left.Numerator * right.Denominator - right.Numerator * left.Denominator,
+                left.Denominator * right.Denominator);
+        }
+
+        public static BigRational operator --(BigRational b) => b - new BigRational(1);
+
+
+    }
+}
+
