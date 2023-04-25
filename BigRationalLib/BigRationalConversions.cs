89c16dd72c3668b039b6e3c611b078c6a8fd2692
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-04-25 10:09:35 +0200

    Add implicit and explicit

---
+++ BigRationalLib/BigRationalConversions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BigRationalLib
+{
+    public readonly partial struct BigRational
+    {
+        public static implicit operator BigRational(long x) => new BigRational(x);
+        public static explicit operator long(BigRational u) => (long)((double)(u.Numerator) / (double)(u.Denominator));
+
+	}
+}
+
