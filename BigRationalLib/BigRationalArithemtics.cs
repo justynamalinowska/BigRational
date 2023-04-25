fc33283bd01814e04729e19139da18da0f1bf205
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-04-25 10:15:12 +0200

    Add “*” and “/” operators

--- BigRationalLib/BigRationalArithemtics.cs
+++ BigRationalLib/BigRationalArithemtics.cs
@@ -29,7 +29,25 @@ namespace BigRationalLib
 
         public static BigRational operator --(BigRational b) => b - 1;
 
+        public static BigRational operator *(BigRational left, BigRational right)
+        {
+            if (left == NaN || right == NaN)
+                return NaN;
 
+            //ToDO - sytuacje specjalne
+
+            return new(left.Numerator * right.Numerator, left.Denominator * right.Denominator);
+        }
+
+        public static BigRational operator /(BigRational left, BigRational right)
+        {
+            if (left == NaN || right == NaN)
+                return NaN;
+
+            //ToDO - sytuacje specjalne
+
+            return new(left.Numerator * right.Denominator, left.Denominator * right.Numerator);
+        }
     }
 }
 
