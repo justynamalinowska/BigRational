879aa517f7f324af66455117639bcbdc24f614cb
Justyna Malinowska <justynamalinowska758@gmail.com>
2023-04-25 09:40:53 +0200

    Create CompareTo() with operators

---
+++ BigRationalLib/BigRationalRelations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace BigRationalLib
+{
+    public readonly partial struct BigRational : IComparable<BigRational>
+    {
+        // u<v
+        // ||
+        //u.CompareTo(v) => 1 (u>v)
+        public int CompareTo(BigRational other)
+        {
+            //ToDo - sytuacje specjalne
+
+            return (this.Numerator * other.Denominator - this.Numerator * other.Denominator).Sign;
+        }
+
+        public static bool operator <(BigRational left, BigRational right)
+        {
+            return left.CompareTo(right) < 0;
+        }
+
+        public static bool operator >(BigRational left, BigRational right)
+        {
+            return left.CompareTo(right) > 0;
+        }
+
+        public static bool operator <=(BigRational left, BigRational right)
+        {
+            return left.CompareTo(right) <= 0;
+        }
+
+        public static bool operator >=(BigRational left, BigRational right)
+        {
+            return left.CompareTo(right) >= 0;
+        }
+    }
+}
+
